be07aa305cd31f520ff7a5812eb2e1facc6a8f41
voduytuan <tuanmaster2012@gmail.com>
2015-04-02 08:07:46 +0700

    Add setting for speedup smarty

--- src/includes/setting.php
+++ src/includes/setting.php
@@ -7,6 +7,12 @@ include('settingtemplate.php');
 //$setting['group']['entry'] = value;
 $setting['site']['cacheKeyPrefix'] = 'lp_';
 
+// For speedup Smarty,
+// If false, the template will not update, so the compiling will ignore
+// So that, it will speed up the process.
+// If true, not enhance performance.
+$setting['site']['smartyCompileCheck'] = true;
+
 $setting['resourcehost']['static'] = 'templates/default/';
 $setting['resourcehost']['static_https'] = 'templates/default/';
 
